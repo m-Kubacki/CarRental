7c2ac0800f17a7e2309fdf1d703030f81c4f4c71
m-Kubacki <michalkubacki02@gmail.com>
2024-04-16 20:04:14 +0200

    2024-04-03 MK

--- carrental/rental/urls.py
+++ carrental/rental/urls.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 from django.urls import path
 from . import views
 
@@ -6,13 +6,4 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('cars/', views.cars, name='cars'),
     path('car/<car_id>', views.car, name='car'),
-=======
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name="index"),
-    path('cars/', views.cars, name='cars'),
-    # path('car/<car_id>', views.car name='car')
->>>>>>> 8b4e9cc810cff4b284d5d53ac73ed907bdb89d13
 ]
