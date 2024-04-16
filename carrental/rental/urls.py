7eae2f625c337ce99fcdb465598104f75a05f30a
m-Kubacki <michalkubacki02@gmail.com>
2024-04-16 19:55:03 +0200

    2024-04-03 MK

--- carrental/rental/urls.py
+++ carrental/rental/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name='index'),
+    path('cars/', views.cars, name='cars'),
+    path('car/<car_id>', views.car, name='car'),
 ]
--- carrental/rental/views.py
+++ carrental/rental/views.py
@@ -3,4 +3,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html.jinja')
+
+def index(request):
+    return render(request, 'cars.html.jinja')
+
+def index(request, car_id):
+    return render(request, 'car.html.jinja' )
