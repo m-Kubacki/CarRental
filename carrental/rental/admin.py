a2f05d8596fd3552c902832d3441e146d6bee380
m-Kubacki <116362784+m-Kubacki@users.noreply.github.com>
2024-06-03 09:17:56 +0200

    29-05-2024 MK

--- carrental/rental/admin.py
+++ carrental/rental/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 from .models import Car
 from .models import Equipment
 # Register your models here.
-admin.site.register(Car)
+admin.site.register(Car, list_display = (
+    'id',
+    'brand',
+    'model',
+    'color', 
+    'price', 
+    'engine_type', 
+    'engine_power', 
+    'gearbox_type', 
+    'available', 
+    'category',
+))
 admin.site.register(Equipment)
